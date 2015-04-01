8db75659e566e31d1040c5dcec8c020d2b5e0c7d
MHISoft <tony.zxue@gmail.com>
2015-03-31 22:30:32 -0400

    renamed input and output file. edit readme

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # youtube-link
-Get the URLs from the HTML source of a youtube page 
+Get the URLs from the HTML source of a youtube page
+
+install the youtube-dl and add to path.
+install the ffmpeg and add to the path, ex: PATH= Z:\media\ffmpeg\bin;[...]
+
+# command
+ high quality audio
+ `z:\app\youtube-dl\youtube-dl.exe -x --audio-format mp3 --audio-quality 0 --no-overwrites -i -a playlist.txt`
+
+download 128bit format audio
+`z:\app\youtube-dl\youtube-dl.exe -x --audio-format mp3 --audio-quality 128K --no-overwrites -i -a playlist.txt`
 
 
-`z:\app\youtube-dl\youtube-dl.exe -x --audio-format mp3 --audio-quality 0 --no-overwrites -i -a youtube-batch.txt`
--- src/main/java/com/mhisoft/youtubelink/GetVideoLinks.java
+++ src/main/java/com/mhisoft/youtubelink/GetVideoLinks.java
@@ -13,8 +13,9 @@ import org.jsoup.select.Elements;
 public class GetVideoLinks {
 
              //<a href="/watch?v=QNJL6nfu__Q&amp;list=PLBF29177C5D2545EF&amp;index=3" class="playlist-video clearfix  spf-link ">
+    //  <a href="/watch?v=ecJymYI6MQU&amp;index=4&amp;list=RDz4ZsQCcitfE" class="playlist-video clearfix  spf-link ">
 
-    String outFile ="S:\\projects\\mhisoft\\youtube-link\\src\\michael-youtube-page-urls.txt";
+    String outFile ="S:\\projects\\mhisoft\\youtube-link\\src\\playlist.txt";
     BufferedWriter writer = null;
     static final String prefix = "https://www.youtube.com";
 
@@ -25,13 +26,17 @@ public class GetVideoLinks {
             e.printStackTrace();
         }
     }
+    //
+    //<a href="/watch?v=4-qyuhsS9oE&amp;list=PL3ED03D57E56D7FB4&amp;index=5" class="playlist-video clearfix  spf-link ">
+    //<a href="/watch?v=kdWQaecFyyo&amp;list=RDHCA9lw4hVcYwk&amp;index=2" class="playlist-video clearfix  spf-link ">
+
 
     public void parseFile(String inputFile) {
         try {
             Document doc = Jsoup.parse(new File(inputFile), "UTF-8");
             Elements links = doc.select("a[href]"); // a with href
             for (Element link : links) {
-                if (link.hasClass("playlist-video")) {
+                if (link.hasClass("spf-link") && link.hasClass("playlist-video")) {
                     String url = link.attributes().get("href");
                     if (url.startsWith("/watch?")) {
                         writer.write(prefix + url);
@@ -45,6 +50,7 @@ public class GetVideoLinks {
         finally {
             try {
                 writer.close();
+                System.out.println("saved to file:" + outFile) ;
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -52,10 +58,17 @@ public class GetVideoLinks {
     }
 
 
+    static String lowQualCmd1 = "z:\\app\\youtube-dl\\youtube-dl.exe -x --audio-format mp3 --audio-quality 128K --no-overwrites -i -a playlist.txt";
+    static String highQualCmd = "z:\\app\\youtube-dl\\youtube-dl.exe -x --audio-format mp3 --audio-quality 0 --no-overwrites -i -a playlist.txt";
+
     public static void main(String[] args) {
         final GetVideoLinks getVideoLinks = new GetVideoLinks();
-        String inputFile = "S:\\projects\\mhisoft\\youtube-link\\src\\michael-youtube-page.txt";
+        String inputFile = "S:\\projects\\mhisoft\\youtube-link\\src\\youtube-html.txt";
         getVideoLinks.parseFile(inputFile);
+        System.out.println("128bit audio download");
+        System.out.println("\t"+lowQualCmd1);
+        System.out.println("High quality audio download:");
+        System.out.println(highQualCmd);
 
     }
 }
